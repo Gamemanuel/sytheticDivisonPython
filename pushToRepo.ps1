890c2ae9870cca1bb4ee9338c83961c81aa2cfa0
Gamemanuel <themathnight@gmail.com>
2025-03-28 10:54:41 -0700

    default

---
+++ pushToRepo.ps1
@@ -0,0 +1,5 @@
+cd C:\Users\gr24265\Documents\sytheticDivisonPython\
+git pull https://github.com/Gamemanuel/sytheticDivisonPython.git
+git add .
+git commit -m "default"
+git push https://github.com/Gamemanuel/sytheticDivisonPython.git
